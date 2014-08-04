092d6b037232cc829c5f7bcfd12258082eda6efd
Michal Wrobel <sparrovv@gmail.com>
2014-08-04 21:53:17 +0100

    Few-liners for creating a homebrew package

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ do
     GO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o "bin/gotr_$GOOS-$GOARCH" gotr.go
   done
 done
+
+# creating a package for homebrew
+mkdir bin/bin
+cp bin/gotr_darwin-amd64 bin/gotr
+tar -czvf gotr-v0.2.0.tar.gz bin/gotr README.md
